d343800ede48b19f05e3e6122c8af5920fbc95e2
suzy <suzyp0223@gmail.com>
2025-03-24 20:27:02 +0900

    topNav- css작업 1차 끝

--- src/components/layout/TopNav.tsx
+++ src/components/layout/TopNav.tsx
@@ -13,50 +13,59 @@ const TopNav: React.FC = (): React.ReactElement => {
   return (
     <>
       <nav className={styles.wrapper}>
+        <div className={styles.loginBarContainer}>
+          <a><div className={styles.login}>로그인</div></a>
+          <a><div className={styles.join}>회원가입</div></a>
+        </div>
+
         <div className={styles.logoContainer}>
           <figure className={styles.logoFig}>
             <a href="">
-              <img src={rumiLogo} alt="로고이미지" style={{ width: '100px', height: '100px' }} />
+              <img src={rumiLogo} alt="로고이미지" />
             </a>
           </figure>
         </div>
 
         <div className={styles.hamBtnContainer}>
-          <figure className={styles.sidebarBtn}>
+          <figure className={styles.hamBtn}>
             <a href="">
-              <img src={hamBtn} alt="ham-btn"  style={{ width: '30px', height: '30px' }}/>
+              {/* 동적으로 마우스 호버시 햄버거버튼 색상변경 */}
+              <img src={hamBtn} alt="ham-btn" />
             </a>
-            <figcaption>카테고리</figcaption>
           </figure>
         </div>
 
         <div className={styles.searchContainer}>
           <input className={styles.searchInput} type="text" placeholder="검색어를 입력하세요" />
           <button className={styles.searchBtn} type="button">
-            <img src={search} alt="검색" style={{ width: '60px', height: '60px' }} />
+            <img src={search} alt="검색" style={{ width: '50px', height: '50px' }} />
           </button>
         </div>
 
         <div className={styles.myContainer}>
           <figure className={styles.myPage}>
             <a href="">
-              <img src={myPage} alt="마이페이지" style={{ width: '24px', height: '24px' }} />
+              <img src={myPage} alt="마이페이지" />
             </a>
             <figcaption>마이페이지</figcaption>
           </figure>
           <figure className={styles.myCart}>
             <a href="">
-              <img src={cart} alt="장바구니" style={{ width: '24px', height: '24px' }} />
+              <img src={cart} alt="장바구니" />
             </a>
             <figcaption>장바구니</figcaption>
           </figure>
         </div>
 
         <ul className={styles.linkList}>
-          <li><a href="/"><img src="" alt="" />여성의류</a></li>
-          <li><a href="/"><img src="" alt="" />남성의류</a></li>
-          <li><a href="/"><img src="" alt="" />유아</a></li>
-          <li><a href="/"><img src="" alt="" />신생아</a></li>
+          <li><a className={styles.linkListA} href="/"><img src="" alt="" />BEST 100</a></li>
+          <li><a className={styles.linkListA} href="/"><img src="" alt="" />NEW 10%</a></li>
+          <li><a className={styles.linkListA} href="/"><img src="" alt="" />SEASON ~90%</a></li>
+          <li><a className={styles.linkListA} href="/"><img src="" alt="" />SET</a></li>
+          <li><a className={styles.linkListA} href="/"><img src="" alt="" />OUTER</a></li>
+          <li><a className={styles.linkListA} href="/"><img src="" alt="" />DRESS</a></li>
+          <li><a className={styles.linkListA} href="/"><img src="" alt="" />TOP</a></li>
+          <li><a className={styles.linkListA} href="/"><img src="" alt="" />BOTTOM</a></li>
         </ul>
       </nav>
     </>
